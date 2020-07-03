93b60fb494562e3473e7d1808e624158e0cc7a97
eutienne <eutienne@ThePrinceps.localdomain>
2020-07-03 11:36:23 +0200

    algo_b is norm prove

--- srcs/algo_b.c
+++ srcs/algo_b.c
@@ -6,11 +6,26 @@
 /*   By: eutrodri <marvin@codam.nl>                   +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/05/17 10:44:24 by eutrodri      #+#    #+#                 */
-/*   Updated: 2020/06/19 22:10:09 by eutrodri      ########   odam.nl         */
+/*   Updated: 2020/07/03 11:33:11 by anonymous     ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/lem_in.h"
+
+static void	make_q(t_link **q, t_able *hashtable)
+{
+	*q = (t_link*)malloc(sizeof(t_link));
+	(*q)->name = ft_strdup(hashtable->array[hashtable->size]->name);
+	(*q)->next = NULL;
+}
+
+static int	give_path_id(t_able *hashtable, int *id, t_link **q2)
+{
+	put_id(hashtable, *id);
+	remove_q2(q2, hashtable);
+	(*id)++;
+	return (1);
+}
 
 t_path		*algo_b(t_able *hashtable, int id, t_path *p)
 {
@@ -23,20 +38,15 @@ t_path		*algo_b(t_able *hashtable, int id, t_path *p)
 	while (i == 1 && hashtable->check == 0)
 	{
 		i = 0;
-		q = (t_link*)malloc(sizeof(t_link));
-		q->name = ft_strdup(hashtable->array[hashtable->size]->name);
-		q->next = NULL;
+		make_q(&q, hashtable);
 		q2 = q;
 		while (q)
 		{
 			room = find_room(hashtable, q->name);
 			if (add_q(&q, room->link, hashtable) == 1)
 			{
-				put_id(hashtable, id);
-				remove_q2(&q2, hashtable);
-				p = save_and_check(p, hashtable, id);
-				id++;
-				i = 1;
+				i = give_path_id(hashtable, &id, &q2);
+				p = save_and_check(p, hashtable, id - 1);
 				break ;
 			}
 			q = q->next;
